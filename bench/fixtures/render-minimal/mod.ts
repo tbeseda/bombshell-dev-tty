976fb9f76284dc5c3bdd7c47e27a6dfbeb13cc76
Nate Moore <git@natemoo.re>
2026-05-30 04:40:39 -0500

    deno fmt

--- bench/fixtures/render-minimal/mod.ts
+++ bench/fixtures/render-minimal/mod.ts
@@ -3,7 +3,7 @@ import { close, grow, open, text } from "../../../ops.ts";
 
 let term = await createTerm({ width: 80, height: 24 });
 term.render([
-    open("root", { layout: { width: grow(), height: grow(), direction: "ttb" } }),
-    text("Hello, World!"),
-    close(),
+  open("root", { layout: { width: grow(), height: grow(), direction: "ttb" } }),
+  text("Hello, World!"),
+  close(),
 ]);
--- bench/fixtures/utils.ts
+++ bench/fixtures/utils.ts
@@ -2,8 +2,11 @@ import { fileURLToPath } from "node:url";
 import { spawnSync } from "node:child_process";
 
 export const fixture = (name: string) => {
-    return new URL(`./${name}/mod.ts`, import.meta.url);
-}
+  return new URL(`./${name}/mod.ts`, import.meta.url);
+};
 
 export const spawnFixture = (name: string) =>
-  spawnSync(process.execPath, [...process.execArgv, fileURLToPath(fixture(name))], { stdio: "ignore" });
+  spawnSync(process.execPath, [
+    ...process.execArgv,
+    fileURLToPath(fixture(name)),
+  ], { stdio: "ignore" });
